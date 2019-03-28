7b3c74debcc272fd7006ec2c9e0320d3d79ed877
Ayush Sharma <ayush.aceit@gmail.com>
2019-03-28 19:23:25 +0100

    chore: adds git config for ci

--- actions/cli/entrypoint.sh
+++ actions/cli/entrypoint.sh
@@ -6,6 +6,10 @@ set -e
 sh -c "npm install -g n"
 sh -c "n -V"
 
+# adding git config
+sh -c "git config --global user.email \"melody@trivago.com\""
+sh -c "git config --global user.name \"melody\""
+
 # For test only
 if [ -n "$REGISTRY_AUTH_TOKEN" ]; then
   echo "ADDING TOKEN"
